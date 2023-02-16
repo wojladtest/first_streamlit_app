5b8ea28a7558d3002d18f99a637e03fdd90195b8
wojladtest <125611478+wojladtest@users.noreply.github.com>
2023-02-16 23:07:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega')
+streamlit.text('Kale')
+streamlit.text('Hard-Boiled Free-Range Egg')
