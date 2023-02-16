2a657bb7039d56840e9aba773b188edb51c55fd5
wojladtest <125611478+wojladtest@users.noreply.github.com>
2023-02-16 23:19:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Mom''''s Healthy Dinner')
+streamlit.title('My Mom\'s Healthy Dinner')
 streamlit.header('Breakfast Favorities')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
