286076319c54a9718f0915b6586632e4508a31b9
wojladtest <125611478+wojladtest@users.noreply.github.com>
2023-02-23 22:04:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 streamlit.title('My Mom\'s Healthy Dinner')
